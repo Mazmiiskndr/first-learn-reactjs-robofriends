dbf6bf94763cc50ffde63ef188c03b1fd69d2879
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-04-06 20:20:40 +0700

    :sparkles: (feat): added errorboundry

---
+++ src/components/ErrorBoundry.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+    this.setState({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "tachyons";
 import App from './containers/App';
+import ErrorBoundary from "./components/ErrorBoundry";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );
